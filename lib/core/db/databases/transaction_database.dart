a68ee89588e8d9a719ab96ffc477721e4fc0208d
Thea Choem <theacheng.g6@gmail.com>
2022-12-13 18:50:54 +0700

    core: add missing objectTransformer to transaction db

--- lib/core/db/databases/transaction_database.dart
+++ lib/core/db/databases/transaction_database.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:spooky/core/db/adapters/base/base_db_adapter.dart';
 import 'package:spooky/core/db/adapters/objectbox/transaction_objectbox_db_adapter.dart';
 import 'package:spooky/core/db/databases/base_database.dart';
 import 'package:spooky/core/db/models/transaction_db_model.dart';
+
+TransactionDbModel _constructTransactionIsolate(Map<String, dynamic> json) {
+  return TransactionDbModel.fromJson(json);
+}
 
 class TransactionDatabase extends BaseDatabase<TransactionDbModel> {
   static final TransactionDatabase instance = TransactionDatabase._();
@@ -15,4 +20,9 @@ class TransactionDatabase extends BaseDatabase<TransactionDbModel> {
 
   @override
   Future<void> onCRUD(TransactionDbModel? object) async {}
+
+  @override
+  Future<TransactionDbModel?> objectTransformer(Map<String, dynamic> json) {
+    return compute(_constructTransactionIsolate, json);
+  }
 }
